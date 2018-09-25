901911d6135732319f2e0eb78a8149b75ef16219
shini <szhang93@ucsc.edu>
2018-09-25 14:39:43 -0700

    file selector added

--- main.py
+++ main.py
@@ -3,9 +3,11 @@
 #https://pythonspot.com/pyqt5/
 
 import sys
+import os
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QMessageBox
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import *
 from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QFileDialog
 
 class App(QMainWindow):
 
@@ -16,19 +18,26 @@ class App(QMainWindow):
         self.top = 10
         self.width = 640
         self.height = 480
+        self.myPath = "~"
         self.initUI()
 
     def testPopup(self):
         QMessageBox.about(self, "Warning!", "Your computer is infected with Ebola")
+
+    def openFile(self):
+        file = QFileDialog.getOpenFileName()
+        print(file)
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
         mainMenu = self.menuBar()
+
         fileMenu = mainMenu.addMenu('File')
         openFile = fileMenu.addAction("Open")
-        openFile.triggered.connect(self.testPopup)
+        openFile.triggered.connect(self.openFile)
+
         editMenu = mainMenu.addMenu('Edit')
         viewMenu = mainMenu.addMenu('View')
         searchMenu = mainMenu.addMenu('Search')
