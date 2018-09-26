3a450b337d72905195d0f2ec6eb2c623141aa05a
shini <szhang93@ucsc.edu>
2018-09-26 14:48:38 -0700

    image and pdf basics

--- main.py
+++ main.py
@@ -4,10 +4,14 @@
 
 import sys
 import os
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QMessageBox
-from PyQt5.QtGui import *
+import tempfile
+# importing required modules
+import PyPDF2
+from pdf2image import convert_from_path, convert_from_bytes
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget, QAction, QTabWidget,QVBoxLayout, QMessageBox
+from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import QFileDialog, QLabel
 
 class App(QMainWindow):
 
@@ -18,19 +22,32 @@ class App(QMainWindow):
         self.top = 10
         self.width = 640
         self.height = 480
-        self.myPath = "~"
         self.initUI()
 
     def testPopup(self):
         QMessageBox.about(self, "Warning!", "Your computer is infected with Ebola")
 
     def openFile(self):
-        file = QFileDialog.getOpenFileName()
-        print(file)
+        myPDF = QFileDialog.getOpenFileName()
+        print(myPDF[0]) #getOpenFileName returns an array
+
+        with tempfile.TemporaryDirectory() as path:
+            images_from_path = convert_from_path(myPDF[0], output_folder=path)
+            print(path)
+            #temp file closed when with is exited
+
+    def displayImg(self,myPath):
+        pixmap = QPixmap(myPath)
+
+        lbl = QLabel(self)
+        lbl.setPixmap(pixmap)
+
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.displayImg("10003762.jpg")
 
         mainMenu = self.menuBar()
 
@@ -44,7 +61,37 @@ class App(QMainWindow):
         toolsMenu = mainMenu.addMenu('Tools')
         helpMenu = mainMenu.addMenu('Help')
 
+        self.table_widget = MyTableWidget(self)
+        self.setCentralWidget(self.table_widget)
+
         self.show()
+
+class MyTableWidget(QWidget):
+
+    def __init__(self, parent):
+        super(QWidget, self).__init__(parent)
+        self.layout = QVBoxLayout(self)
+
+        # Initialize tab screen
+        self.tabs = QTabWidget()
+        self.tab1 = QWidget()
+        self.tab2 = QWidget()
+        self.tabs.resize(300,200)
+
+        # Add tabs
+        self.tabs.addTab(self.tab1,"Tab 1")
+        self.tabs.addTab(self.tab2,"Tab 2")
+
+        # Create first tab
+        #self.tab1.layout = QVBoxLayout(self)
+        self.pushButton1 = QPushButton("PyQt5 button")
+        #self.tab1.layout.addWidget(self.pushButton1)
+        #self.tab1.setLayout(self.tab1.layout)
+
+        # Add tabs to widget
+        #self.layout.addWidget(self.tabs)
+        #self.setLayout(self.layout)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
